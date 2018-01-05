6ec4aee53724a496df436b791c39770d477104cd
Katerina <t.ekaterina.y@gmail.com>
2018-01-05 12:27:19 +0300

    WebDriver_test

---
+++ readme.md
@@ -0,0 +1,21 @@
+# WebDriver JS
+#1. Select a system under test (preferable: web application being tested during your project activities);
+#2.	Agree with your mentor and document a scenario to automate. The scenario should be linear (no need to #   implement complex logic for now) and contain 8-10 simple steps (open a page, send keys, click, etc);
+#3.	Use WebDriver API as much as possible (browser navigation, clicks, switchTo and etc.);
+#4.	Use several locator strategies i.e. different types of locators (and select the most suitable in your #   case);
+#5.	Experiment with waits (implicit and explicit).
+
+# Scenario
+# Step 1. Open application
+# Step 2. Find Registration element
+# Step 3. Click on Registration
+# Step 4. Fill all fields 
+# Step 5. Find Register button
+# Step 6. Click Register button
+# Step 7. Switch to another tab
+# Step 8. Open email
+# Step 9. Wait until new message will come / Refresh page
+# Step 10. Find confirmation letter
+# Step 11. Open confirmation letter
+# Step 12. Click the link
+# Step 13. Check confirmation message is shown
---
+++ test.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var webdriver = require('selenium-webdriver');
+var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
+webdriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+function ErrorHandling(err) {
+     console.log('Oops...Something went wrong!\n', err.stack, '\n');
+     quitDriver();
+}
+
+browser.get('http://www.novatek.by/');
+browser.findElement(webdriver.By.css('[href^="/auth/registration/"]')).click();
+browser.findElement(webdriver.By.name("REGISTER[NAME]")).sendKeys("Yasya Pupkin");
+browser.findElement(webdriver.By.name("REGISTER[PERSONAL_PHONE]")).sendKeys("291111111");
+browser.findElement(webdriver.By.name("REGISTER[EMAIL]")).sendKeys("temporary@p33.org");
+browser.findElement(webdriver.By.name("REGISTER[PASSWORD]")).sendKeys("654321qwe");
+browser.findElement(webdriver.By.name("REGISTER[CONFIRM_PASSWORD]")).sendKeys("654321qwe");
+browser.findElement(webdriver.By.xpath("//form[@id='registraion-page-form']/div[3]/div/div[2]")).click()
+browser.navigate().to("https://temp-mail.ru/");
+browser.findElement(webdriver.By.id("click-to-change")).click();
+browser.findElement(webdriver.By.id("mail")).sendKeys("temporary");
+browser.findElement(webdriver.By.id("postbut")).click();
+browser.findElement(webdriver.By.id("click-to-refresh")).click();
+browser.findElement(webdriver.By.partialLinkText("Confirmation")).click();
+	then(function(links){
+		console.log('Found', links.length, 'Wiki links.' )
+browser.quit();
